671184e8104e3c537259c865c0451f2066042f1f
Zohaib521321 <khanzohaib3231@gmail.com>
2024-08-07 17:30:30 +0500

    getting url

--- multiple_pages.py
+++ multiple_pages.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-response=requests.get("https://www.flipkart.com/search?q=aptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off"+"&page=3")
+response=requests.get("https://www.flipkart.com/search?q=mobile&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off")
 print(response)
 soup=BeautifulSoup(response.text,"html.parser")
 np=soup.find("a",class_="_9QVEpD").get("href")
